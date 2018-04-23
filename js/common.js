66e9bcc4d2a0f44adcaee49e9815553d397d4a2d
n.yerdenqyzy <n.yerdenqyzy@bugingroup.kz>
2018-04-23 19:02:18 +0600

    Signed-off-by: n.yerdenqyzy <n.yerdenqyzy@bugingroup.kz>

---
+++ js/common.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+$('.slid-gallery ').slick({
+    arrows:false,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    variableWidth: true
+});
+$('.slid-ser').slick({
+    arrows:true,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    variableWidth: true
+});
---
+++ js/jquery-plugin.js
@@ -0,0 +1,84 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        //Navigation
+        menu: '#menu',
+        lockAnchors: false,
+        anchors:['home', 'service','price', 'gallery','callback'],
+        navigation: true,
+        navigationPosition: 'left',
+        navigationTooltips: ['Главная', 'Наши услуги','Цены', 'Галерея','Обратный звонок'],
+        showActiveTooltip: false,
+        slidesNavigation: false,
+        slidesNavPosition: 'bottom',
+
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 1000,
+        autoScrolling: true,
+        fitToSection: true,
+        fitToSectionDelay: 1000,
+        scrollBar: false,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        continuousHorizontal: false,
+        scrollHorizontally: false,
+        interlockedSlides: false,
+        dragAndMove: false,
+        offsetSections: false,
+        resetSliders: false,
+        fadingEffect: false,
+        normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        scrollOverflowReset: false,
+        scrollOverflowOptions: null,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+        bigSectionsDestination: null,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
+
+        //Design
+        controlArrows: true,
+        verticalCentered: true,
+        // paddingTop: '3em',
+        // paddingBottom: '10px',
+        fixedElements: '#header, .footer',
+        responsiveWidth: 767,
+        responsiveHeight: 0,
+        responsiveSlides: false,
+        parallax: false,
+        parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+
+        //Custom selectors
+        sectionSelector: '.item-section',
+        slideSelector: '.slide',
+
+        lazyLoading: true,
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterResponsive: function(isResponsive){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+    });
+});
+jQuery(function($) {
+    $('.pie_progress').asPieProgress({
+        namespace: 'pie_progress',
+        min: 0,
+        max: 100,
+        speed: 100,
+        easing: 'linear'
+    });
+    $('.pie_progress').asPieProgress('start');
+});
